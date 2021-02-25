7a796d6f958a09cdf3f006ff10d8be3fb9901d8e
student <lddavila@miners.utep.edu>
2021-02-24 22:36:29 -0700

    third attempt

--- src/checkIfWhiteSpace.c
+++ src/checkIfWhiteSpace.c
@@ -2,14 +2,20 @@
 
 int space_char(char c)
 {
-  if(c == ' ')
+  if(c == ' '){
     printf("This is a space\n");
-  else if (c ==9)
+    return 1; 
+  }
+  else if (c ==9){
     printf("This is a tab\n");
-  else if (c ==10)
+    return 1;
+    else if (c ==10){
     printf("This is a new line\n");
-  else
-    printf("This is not a white space char.\n"); 
+    return 1;}
+    else{
+    printf("This is not a white space char.\n");
+    return 0;
+    }
 }
 main(){
   printf("Please enter a char for testing.\n");
