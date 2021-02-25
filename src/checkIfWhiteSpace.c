c2b25f9f049335400804c533ed16202d881edadd
student <lddavila@miners.utep.edu>
2021-02-24 22:31:57 -0700

    second attempt

--- src/checkIfWhiteSpace.c
+++ src/checkIfWhiteSpace.c
@@ -1,2 +1,17 @@
-include <stdio.h>
+#include <stdio.h>
 
+int space_char(char c)
+{
+  if(c == ' ')
+    printf("This is a space\n");
+  else if (c ==9)
+    printf("This is a tab\n");
+  else if (c ==10)
+    printf("This is a new line\n");
+  else
+    printf("This is not a white space char.\n"); 
+}
+main(){
+  printf("Please enter a char for testing.\n");
+  space_char(getchar()); 
+}
