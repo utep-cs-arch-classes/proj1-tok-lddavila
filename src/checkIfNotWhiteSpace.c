223148e87c8ae55b6c03f398422e4aefd4705722
student <lddavila@miners.utep.edu>
2021-02-26 23:39:22 -0700

    No printout and no main version

--- src/checkIfNotWhiteSpace.c
+++ src/checkIfNotWhiteSpace.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
-
 int non_space_char(char c){
-  if(c == ' '){
-    printf("This is a space char (space).\n");
+  if (c == ' ')
     return 0;
-  }
-  else if(c == 9){
-    printf("This is a space char(tab).\n");
+  else if( c == 9)
     return 0;
-  }
-  else if(c==10){
-    printf("This is a space char(new line). \n");
+  else if (c == 10)
     return 0;
-  }
-  else{
-    printf("This is not a white space char.\n");
+  else
     return 1;
-
 }
-}
-main(){
-  printf("Please enter a char for testing.\n");
-  non_space_char(getchar());
-  }
