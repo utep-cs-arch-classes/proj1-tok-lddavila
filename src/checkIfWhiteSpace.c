e365f238eebfe27de32e7192d694668c319d7918
student <lddavila@miners.utep.edu>
2021-02-27 00:44:11 -0700

    Removed print statements and main from checkIfWhiteSpace

--- src/checkIfWhiteSpace.c
+++ src/checkIfWhiteSpace.c
@@ -3,21 +3,22 @@
 int space_char(char c)
 {
   if(c == ' '){
-    printf("This is a space\n");
+    /*intf("This is a space\n");*/
     return 1; 
   }
   else if (c ==9){
-    printf("This is a tab\n");
-    return 1;
-    else if (c ==10){
-    printf("This is a new line\n");
+    /*intf("This is a tab\n");*/
     return 1;}
-    else{
-    printf("This is not a white space char.\n");
+  else if (c ==10){
+    /* printf("This is a new line\n");*/
+    return 1;}
+  else{
+    /* printf("This is not a white space char.\n");*/
     return 0;
     }
 }
+/*
 main(){
   printf("Please enter a char for testing.\n");
   space_char(getchar()); 
-}
+  }*/
