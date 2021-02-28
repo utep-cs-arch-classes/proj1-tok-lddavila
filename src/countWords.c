1561c82e83ba965c8cfe3e702daf4699d1d9e2d5
student <lddavila@miners.utep.edu>
2021-02-27 17:35:00 -0700

    Hessitantly functional countWords.c

---
+++ src/countWords.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "tokenizer.h"
+#include <string.h>
+int count_words(char *str){
+  int counter = 0; 
+  printf("The first symbol of the string before going into the loop: %c\n", *str); 
+  while(*str != 10) {
+    
+    str = word_start(str);
+    printf("The symbol the pointer points to after calling word_start(str): %c\n", *str);
+    counter++;
+    str = word_end(str);
+    printf("The symbol the pointer points to after calling word_end(str): %d\n", *str);
+  }
+  return counter; 
+}
+/*
+main(){
+  printf("Pleas eneter the text. \n");
+  char str[10];
+  fgets(str,10,stdin);
+  int numOfWords;
+  numOfWords = count_words(str);
+  printf("The text you just entered has this many words: %d\n", numOfWords); 
+}
+*/
