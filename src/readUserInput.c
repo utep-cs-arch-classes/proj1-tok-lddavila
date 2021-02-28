a2411d50f76dc3cce8072ad361a639f7dc54ba70
student <lddavila@miners.utep.edu>
2021-02-27 18:04:29 -0700

    Created Make File

--- src/readUserInput.c
+++ src/readUserInput.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
+#include "tokenizer.h"
 void main(){
-  printf("Please enter something. \n");
-  int userInput = getchar();
-  printf("You just enetered: " );
-  while(userInput != EOF){
-    putchar(userInput);
-    userInput = getchar();
-  }
+  printf("Please enter something.\n");
+  char str[10];
+  fgets(str, 10, stdin);
+  int a = count_words(str);
+  printf("This String has the following amount of words: %d\n", a);
 }
