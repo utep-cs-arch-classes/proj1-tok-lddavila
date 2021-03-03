87ad68dadc9c962a612ecce730b2979ac2b5336d
student <lddavila@miners.utep.edu>
2021-03-02 20:18:50 -0700

    cleaned up the comments

--- src/copy.c
+++ src/copy.c
@@ -3,28 +3,6 @@
 #include "tokenizer.h"
 
 char *copy_str(char *src, short len){
-  /* int counter = (int) len;
-  printf("What the counter should be assigned to: %d\n", counter);
-  char a ='c';
-  char *ptrWeCopyTo = &a;
-  char *ptrWeReturn;
-  ptrWeReturn = ptrWeCopyTo; 
-  printf("What the pointer should be pointing to: %c\n", *src); 
-  while(counter > 0){
-     printf("Does it get here\n" );
-    *ptrWeCopyTo = *src;
-    /* printf("You can assign the value in *src to *ptr.\n");
-    ++ptrWeCopyTo;
-    /* printf("You can increment ptr.\n");
-    ++src;
-    /* printf("You can increment src.\n");
-  
-    counter = counter -1;
-    /* printf("You can decrement counter.\n");
-  }
-  printf("%c\n", *ptrWeReturn);
-  return ptrWeReturn;
-*/
 
     char *ptrToBeReturned;
     ptrToBeReturned = (char *) malloc((int)len+1);
