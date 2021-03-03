cae40a6430701fbd8e4dfc5ca8087bd1eae3fd93
student <lddavila@miners.utep.edu>
2021-03-02 18:07:01 -0700

    tentative copy function

--- src/copy.c
+++ src/copy.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include "tokenizer.h"
+
+char *copy_str(char *src, short len){
+  int counter = (int) len;
+  char *ptr;
+  while(counter >0 ){
+    *ptr = *src;
+    ++ptr;
+    ++src; 
+    counter-=1;
+  }
+  return ptr; 
+}
